b081c628320de74473d3293e7be3d8424b2152cc
Joshua Sanders <ninjosh2@gmail.com>
2020-01-08 03:33:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
   end 
   
   def list_pets 
-    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cats"
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)"
   
   
   
