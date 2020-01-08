382ff79e4e4beddc29fe06df498dcdacf9932339
Joshua Sanders <ninjosh2@gmail.com>
2020-01-08 02:42:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species 
+  
+  @@all = [] 
+  
+  def initilize(name) 
+    @name = name 
+    @species = "human"
+    @@all << self 
+  end 
+  
+  def say_species
+    "I am a #{self.species}."
+  end 
 end
